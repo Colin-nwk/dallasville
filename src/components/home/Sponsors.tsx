69f419af2aff7db246e83b5408b66bc1af8d123b
Collins Nwoko C <collinsnwoko05@gmail.com>
2023-07-18 13:21:42 +0100

    minor edit

--- src/components/home/Sponsors.tsx
+++ src/components/home/Sponsors.tsx
@@ -7,7 +7,7 @@ const Sponsors = () => {
     <>
       <div className="border-t border-b my-10">
         <section className="container  mx-auto p-4 md:p-10 w-full h-fit">
-          <div className="flex justify-start md:justify-between items-center gap-4 flex-wrap ">
+          <div className="flex justify-start md:justify-between items-center gap-4 flex-wrap no-scroll ">
             <div>
               <h4 className="text-3xl font-bold ">As featured in</h4>
             </div>
@@ -15,22 +15,22 @@ const Sponsors = () => {
             <img
               src={punch}
               alt="punch"
-              className="w-fit h-fit object-contain aspect-auto"
+              className="w-28 h-fit object-contain aspect-auto"
             />
             <img
               src={guardian}
               alt="guardian"
-              className="w-fit h-fit object-contain aspect-auto"
+              className="w-32 h-fit object-contain aspect-auto"
             />
             <img
               src={buj}
               alt="buj"
-              className="w-fit h-fit object-contain aspect-auto"
+              className="w-28 h-fit object-contain aspect-auto"
             />
             <img
               src={ontex}
               alt="ontex"
-              className="w-fit h-fit object-contain aspect-auto"
+              className="w-28 h-fit object-contain aspect-auto"
             />
           </div>
         </section>
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -22,8 +22,8 @@ const About = () => {
             alt="luxuzy house with pool"
             className="w-full h-full object-cover absolute mix-blend-overlay -z-10"
           />
-          <div className="container mx-auto px-4 md:px-10 w-ful w-full  -z-30 ">
-            <div className=" container grid grid-cols-4  w-full h-full absolute top-28 md:top-14 ">
+          <div className="container mx-auto px-4 md:px-10 w-ful w-full  -z-20 ">
+            <div className=" container grid grid-cols-4  w-full h-full absolute top-28 md:top-14 -z-20">
               <div className="border-l border-white/20 border-r"></div>
               <div className="border-r border-white/20"></div>
               <div className="border-r border-white/20"></div>
