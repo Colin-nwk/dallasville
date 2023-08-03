ffa1b56887bbf6913096bb2fa5b77846da87feef
Collins Nwoko C <collinsnwoko05@gmail.com>
2023-08-03 10:12:04 +0100

    minor edit banner

--- src/components/projects/ProjectDetails.tsx
+++ src/components/projects/ProjectDetails.tsx
@@ -133,7 +133,7 @@ const ProjectDetails = () => {
                     />
 
                     <div className=" absolute top-0 left-0 text-white grid grid-cols-2 md:grid-cols-3 gap-4 justify-start items-center px-4 py-16 sm:py-6 lg:py-10 lg:px-8 h-full w-full md:ml-[60px] ">
-                      <div className="grid grid-cols-1 grid-rows-4 justify-between items-start gap-6 md:gap-10 h-full w-full col-span-1">
+                      <div className="grid grid-cols-1 grid-rows-4 justify-center items-center gap-6 md:gap-10 h-full w-full col-span-1">
                         <div className=" flex flex-col justify-start items-start gap-2">
                           <p className="text-sm md:text-base">Type</p>
                           <h4 className="text-lg md:text-2xl">Residential</h4>
